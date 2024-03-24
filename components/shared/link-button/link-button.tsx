314769a060ca90028c985d638d0b28251c6a1a04
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-03-24 13:25:24 +0500

    converted px to rems

--- components/shared/link-button/link-button.tsx
+++ components/shared/link-button/link-button.tsx
@@ -18,26 +18,28 @@ type ActionProps = BaseProps &
   );
 
 const LinkButton: FC<ActionProps> = ({ variant, styles, text, ...props }) => {
+  //    lg-1 styles="!max-w-[31.375rem] !h-[3.853rem] hover:!rounded-[2.75rem] before:!hover:rounded-[2.75rem] !font-semibold !text-[1.208rem] !leading-[1.462rem]"
+
   const getSizeClass = () => {
     switch (variant) {
       case "rounded":
-        return "max-w-[240px] w-full h-[56px] rounded-full before:hover:rounded-full font-light text-[1.5rem] leading-[1.8rem]";
+        return "max-w-[15rem] w-full h-[3.5rem] rounded-full before:hover:rounded-full font-light text-[1.5rem] leading-[1.8rem]";
       case "corner":
-        return "max-w-[238px] w-full h-[54px] rounded-md rounded-bl-[24px] hover:rounded-[34px] before:hover:rounded-[34px] font-medium text-xl leading-[1.525rem]";
+        return "max-w-[14.875rem] w-full h-[3.375rem] rounded-md rounded-bl-[1.5rem] hover:rounded-[2.125rem] before:hover:rounded-[2.125rem] font-medium text-xl leading-[1.525rem]";
 
       case "sm":
-        return "max-w-[130px] w-full h-[44px] rounded-lg before:-inset-y-[10px] before:-inset-x-[11px] hover:rounded-[38px] before:hover:rounded-[38px] font-semibold text-sm leading-[1.375rem]";
+        return "max-w-[8.125rem] w-full h-[2.75rem] rounded-lg before:-inset-y-[0.625rem] before:-inset-x-[0.688rem] hover:rounded-[2.375rem] before:hover:rounded-[2.375rem] font-semibold text-sm leading-[1.375rem]";
 
       case "lg-1":
       case "lg":
-        return "max-w-[515px] w-full h-[51px] rounded-[8px] hover:rounded-[34px] before:hover:rounded-[34px] font-semibold text-base leading-[1.21rem]";
+        return "max-w-[32.188rem] w-full h-[3.188rem] rounded-[0.5rem] hover:rounded-[2.125rem] before:hover:rounded-[2.125rem] font-semibold text-base leading-[1.21rem]";
 
       default:
         return "";
     }
   };
 
-  const allClassNames = `inline-block bg-indigo-blue border-indigo-blue before:cotent-[''] before:hover:border-indigo-blue relative m-[16px] mt-10 border-[4px] text-lg transition-all duration-700 ease-in-out before:absolute before:-inset-[10.5px] before:border-transparent  before:transition-all before:duration-700 hover:bg-[rgba(28,48,107,0.85)]  before:hover:border-[4px] before:z-1 ${getSizeClass()} ${styles}`;
+  const allClassNames = `inline-block bg-indigo-blue border-indigo-blue before:cotent-[''] before:hover:border-indigo-blue relative m-[16px] mt-10 border-[0.25rem] text-lg transition-all duration-700 ease-in-out before:absolute before:-inset-[0.656rem] before:border-transparent  before:transition-all before:duration-700 hover:bg-[rgba(28,48,107,0.85)]  before:hover:border-[0.25rem] before:z-1 ${getSizeClass()} ${styles}`;
 
   if (props.as === "link") {
     const { as, ...rest } = props;
