d57f484b676108799ed1b79bd502ff4b7553ad66
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-03-25 09:09:14 +0500

    removed margins from buttons

--- app/page.tsx
+++ app/page.tsx
@@ -8,23 +8,5 @@ import LinkButton from "@/components/shared/link-button/link-button";
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-[#F5F5F5]">
-      <LinkButton as="button" variant="rounded" text="Get Quote Now" />
-      <LinkButton as="button" variant="corner" text="Get a Quote" />
-      <LinkButton as="button" variant="sm" text="Contact Us" />
-      <LinkButton as="button" variant="lg" text="Send Message" />
-      <LinkButton
-        as="button"
-        variant="lg"
-        styles="!max-w-[31.375rem] !h-[3.853rem] hover:!rounded-[2.75rem] before:!hover:rounded-[2.75rem] !font-semibold !text-[1.208rem] !leading-[1.462rem]"
-        text="Send Message lg-1"
-      />
-
-      <Dealers />
-      <AdvertisementPolicy />
-      <SkillSet />
-      <Footer />
-    </main>
-  );
+  return <main className="min-h-screen bg-[#F5F5F5] px-10"></main>;
 }
