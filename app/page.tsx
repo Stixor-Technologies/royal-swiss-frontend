2ecc8d8a6776ef24c79c9cf4ba11793f93666e1c
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-04-03 12:05:15 +0500

    added company projects component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import HomePageProjects from "@/components/home-page/projects/projects";
 
 export default function Home() {
-  return <div className="container relative min-h-screen"></div>;
+  return (
+    <div className="container relative min-h-screen">
+      <HomePageProjects />
+    </div>
+  );
 }
---
+++ components/home-page/projects/projects.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const HomePageProjects = () => {
+  return (
+    <section className="container mt-10 md:mt-[6.875rem]">
+      <div className="text-center md:text-left">
+        <h2 className="mb-6 font-righteous text-4xl leading-[2.7rem]  text-indigo-blue md:mb-5 md:text-5xl md:leading-[3.6rem]">
+          Our Projects
+        </h2>
+
+        <p className="text-[0.813rem] font-light leading-[1.056rem] text-gray md:text-[1.375rem] md:leading-[1.829rem]">
+          Lorem ipsum dolor sit amet consectetur. Faucibus semper id porta
+          egestas mi praesent. In ultrices blandit risus elementum habitant
+          ipsum at augue. Amet cursus ornare neque quam. Accumsan neque
+          elementum ut nisl diam congue ornare. Pulvinar.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default HomePageProjects;
