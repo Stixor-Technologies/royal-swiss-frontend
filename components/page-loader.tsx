bd75baa691410418a8df09e8c46d2c8bccc3e677
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-04-16 17:38:47 +0500

    removed unused code

--- components/page-loader.tsx
+++ components/page-loader.tsx
@@ -5,11 +5,6 @@ import Logo from "../public/logo.svg";
 
 const PageLoader = () => {
   return (
-    // <div
-    //   id="transition-element"
-    //   className="fixed inset-x-0 bottom-0 top-0 z-50 w-screen rounded-b-3xl bg-indigo-blue"
-    // />
-
     <div
       id="transition-element"
       className="fixed inset-x-0 bottom-0 top-0 z-50 flex w-screen items-center justify-center rounded-b-3xl bg-indigo-blue"
