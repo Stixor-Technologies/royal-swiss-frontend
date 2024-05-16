348281672f0ba1173664d86af05688369176d556
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-05-16 19:22:38 +0500

    fixed the validaiton on contact form - trailing spaces

--- utils/formik-schema.ts
+++ utils/formik-schema.ts
@@ -2,10 +2,11 @@ import * as Yup from "yup";
 
 export const ContactFormSchema = Yup.object().shape({
   name: Yup.string()
-    .matches(/^[A-Za-z\s]+$/, "Only characters are allowed")
+    // .matches(/^[A-Za-z\s]+$/, "Only characters are allowed")
+    .required("Please enter your name")
+    .matches(/^[^\s]+(?:\s[^\s]+)*$/, "Name cannot start/end with space")
     .min(3, "Name should be atleast 3 characters long.")
-    .max(20, "Name should be at most 30 characters long.")
-    .required("Please enter your name"),
+    .max(20, "Name should be at most 30 characters long."),
 
   email: Yup.string()
     .matches(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i, "Invalid email address")
@@ -16,9 +17,10 @@ export const ContactFormSchema = Yup.object().shape({
     .matches(/^(\+92|0|92)[0-9]{10}$/, "Phone number is not valid"),
 
   subject: Yup.string()
+    .required("Please enter subject")
     .min(3, "Subject should be atleast 3 characters long.")
     .max(20, "Subject should be at most 20 characters long.")
-    .required("Please enter subject"),
+    .matches(/^[^\s]+(?:\s[^\s]+)*$/, "Subject cannot start/end with space"),
 
   message: Yup.string()
     .required("Please enter your message")
