2096793ae49ebb19b183059456cd145ae4dda8c8
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-05-20 01:06:33 +0500

    fixed validations on contact form

--- utils/formik-schema.ts
+++ utils/formik-schema.ts
@@ -3,9 +3,13 @@ import * as Yup from "yup";
 export const ContactFormSchema = Yup.object().shape({
   name: Yup.string()
     .required("Please enter your name")
-    .matches(/^[^\s]+(?:\s[^\s]+)*$/, "Name cannot start/end with space")
     .min(3, "Name should be atleast 3 characters long.")
-    .max(20, "Name should be at most 30 characters long."),
+    .max(20, "Name should be at most 30 characters long.")
+    .matches(/^[A-Za-z\s]+$/, "Only characters are allowed")
+    .matches(
+      /^[^\s]+(?:$|.*[^\s]+$)/,
+      "First name cannot start/end with space",
+    ),
 
   email: Yup.string()
     .matches(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i, "Invalid email address")
@@ -18,11 +22,11 @@ export const ContactFormSchema = Yup.object().shape({
   subject: Yup.string()
     .required("Please enter subject")
     .min(3, "Subject should be atleast 3 characters long.")
-    .max(20, "Subject should be at most 20 characters long.")
+    .max(50, "Subject should be at most 50 characters long.")
     .matches(/^[^\s]+(?:\s[^\s]+)*$/, "Subject cannot start/end with space"),
 
   message: Yup.string()
     .required("Please enter your message")
     .min(10, "Message should be atleast 10 characters long.")
-    .max(200, "Message should be at most 300 characters long."),
+    .max(500, "Message should be at most 500 characters long."),
 });
